4a40b98a6d467089cb3a10eb24d09acfb92707e7
CheBar1 <chebarclay@live.com.au>
2022-10-30 16:58:04 +1030

    updated JS to generate current weather items

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # weather-dashboard
 
 ## Description
+Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. This challenge was to build a weather dashboard that runs in the browser and feature dynamically updated HTML and CSS.
 
+My challenge was to use the 5 Day Weather Forecast to retrieve weather data for cities. I was advised to use the base URL: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}.  
+*It should be noted as per https://openweathermap.org/api/uvi that the ultraviolet index was retired from the above URL 1st April 2021, hence I have not provided the UV Index for the current weather.
+
+In order to practice my foundational skills I determined that I would code this challenge using only HTML, CSS, jQuery and JavaScript - hence no CSS frameworks have been utilised.
+
+## User Story
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Acceptance Criteria
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
 
 ## Screen Shot
 The following images show the web application's appearance and functionality:
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,22 @@
-// 2 fetch requests
-// 1. Search by city name to get lat and lon values
-// https://openweathermap.org/api/geocoding-api
-
-// 2. Use lon/lat from first request to get weather
-// https://openweathermap.org/api
-
-const API_KEY = "2f79d354118140a37bb6982e94319d26";
-
-var cityInput = document.querySelector("#search-city");
-const cityNameEl = document.querySelector("#city-name");
-const searchButton = document.querySelector('#search-button');
-
-var today = moment();
-$("#date").text(today.format('(L)'))
+const cityInputEL = document.querySelector("#search-city");
+let cityNameEl = document.querySelector("#city-name");
+const searchButtonEl = document.querySelector('#search-button');
+const clearButtonEl = document.querySelector('#clear-button');
+const currentWeatherItemsEl = document.querySelector('#current-weather-items');
+const searchHistoryEl = document.querySelector('#search-container');
+const tempEl = document.querySelector('#temp');
+const windEl = document.querySelector('#wind');
+const humidityEl = document.querySelector('#humidity');
 
 
-function getWeatherData(event){
+let today = moment(); $("#date").text(today.format('(L)'))
+
+
+function getWeatherData(event) {
     event.preventDefault();
-    
-    const cityName = cityInput.value;
+ 
+//Search for Open Weather Geocoding API city name the user inputs to obtain latitude and longitude
+    const cityName = cityInputEL.value;
     const requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=2f79d354118140a37bb6982e94319d26`;
 
     $("#date").text(cityName + " " +$("#date").text())
@@ -28,11 +26,12 @@ function getWeatherData(event){
         return response.json();
     })
     .then(function(data){
-        console.log(data); 
-        console.log(data[0].lat);  
-        console.log(data[0].lon); 
+        // console.log(data[0].lat); Console.log to check outcome 
+        // console.log(data[0].lon); Console.log to check outcome 
 
-        const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=2f79d354118140a37bb6982e94319d26`;
+//Search Open Weather 5 day weather forecast 2.5 for current weather details at top of page
+        let weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=2f79d354118140a37bb6982e94319d26&units=metric`;
+        // let iconLink = "https://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png"
         
         fetch(weatherUrl)
         .then(function(response){
@@ -40,25 +39,45 @@ function getWeatherData(event){
         })
         .then(function(data){
             console.log(data);
-            // console.log(data[].)
+            // console.log(data.list[0].main.temp); Console.log to check outcome 
+            // console.log(data.list[0].wind.speed); Console.log to check outcome 
+            // console.log(data.list[0].main.humidity); Console.log to check outcome 
+                document.querySelector('#temp').textContent="Temp: " + data.list[0].main.temp + " °C";
+                document.querySelector('#wind').textContent="Wind: " + data.list[0].wind.speed + " KPH";
+                document.querySelector('#humidity').textContent="Humidity: " + data.list[0].main.humidity + " %";
+
+            // for(i=0;i<40;i+=8){
+            
+            //     document.querySelector('#temp').innerText=data.list[i].main.temp;
+            //     document.querySelector('#wind').innerText=data.list[i].wind.speed;
+            //     document.querySelector('#humidity').innerText=data.list[i].main.humidity;
+                
+            // }
         })
     })
 }
 
+let clearHistory = function() {
+    search = [];
+    localStorage.clear();
+    loadSearch();
+}
 
-searchButton.addEventListener("click",getWeatherData)
+//search for location that is clicked on in history
+let reSearch = function(event) {
+    if (event.target.innerHTML.includes("<")) {
+        return;
+    } else {
+        getCoordinates(event.target.innerHTML)
+    }
+}
 
-var weatherApi = "api.openweathermap.org/data/2.5/forecast?lat=''&lon=''&appid=2f79d354118140a37bb6982e94319d26";
+searchButtonEl.addEventListener("click", getWeatherData);
+clearButtonEl.addEventListener("clcik", clearHistory);
+searchHistoryEl.addEventListener("click", reSearch);
+ 
 
  
-var currentWeatherItems = document.getElementById('current-weather-items')
-
  
-
-// Obtain user selected city latitude and longitude from Geocoding API
-
-// Obtains user selected  city weather from Open Weather API
-
-// Declaring the function and fetching the weather API 
  
 
